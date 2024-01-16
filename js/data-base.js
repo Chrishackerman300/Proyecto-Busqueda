bcf848c756dde8c6e9a017cb505cdb61671d652f
Christian Mendoza <christiancr7881@gmail.com>
2024-01-15 21:13:18 -0600

    Base de Datos

---
+++ js/data-base.js
@@ -0,0 +1,272 @@
+//Base de Datos de los Autos
+const autos = [
+    {
+        marca: 'Audi',
+        year: 2014,
+        minimo: 55000,
+        puertas: 2,
+        transmision: 'Estandar',
+        color: 'Negro'
+    },
+
+    {
+        marca: 'Mercedes Benz',
+        year: 2020,
+        minimo: 80000,
+        puertas: 2,
+        transmision: 'Automatico',
+        color: 'Blanco'
+    },
+
+    {
+        marca: 'Ferrari',
+        year: 2017,
+        minimo: 60000,
+        puertas: 2,
+        transmision: 'Automatico',
+        color: 'Rojo'
+    },
+
+    {
+        marca: 'Lamborghini',
+        year: 2015,
+        minimo: 55000,
+        puertas: 2,
+        transmision: 'Automatico',
+        color: 'Amarillo'
+    },
+
+    {
+        marca: 'Honda',
+        year: 2015,
+        minimo: 42000,
+        puertas: 4,
+        transmision: 'Automatico',
+        color: 'Amarillo'
+    },
+
+    {
+        marca: 'Nissan',
+        year: 2014,
+        minimo: 40000,
+        puertas: 4,
+        transmision: 'Estandar',
+        color: 'Azul'
+    },
+
+    {
+        marca: 'Ford',
+        year: 2014,
+        minimo: 50000,
+        puertas: 2,
+        transmision: 'Estandar',
+        color: 'Naranja'
+    },
+
+    {
+        marca: 'Volks Wagen',
+        year: 2018,
+        minimo: 60000,
+        puertas: 4,
+        transmision: 'Estandar',
+        color: 'Negro'
+    },
+
+    {
+        marca: 'Dodge',
+        year: 2019,
+        minimo: 55000,
+        puertas: 4,
+        transmision: 'Estandar',
+        color: 'Rojo'
+    },
+
+    {
+        marca: 'Kia',
+        year: 2024,
+        minimo: 65000,
+        puertas: 4,
+        transmision: 'Automatico',
+        color: 'Azul'
+    },
+
+    {
+        marca: 'Honda',
+        year: 2021,
+        minimo: 70000,
+        puertas: 2,
+        transmision: 'Automatico',
+        color: 'Naranja'
+    },
+
+    {
+        marca: 'Audi',
+        year: 2020,
+        minimo: 89000,
+        puertas: 2,
+        transmision: 'Estandar',
+        color: 'Blanco'
+    },
+
+    {
+        marca: 'Nissan',
+        year: 2019,
+        minimo: 60000,
+        puertas: 4,
+        transmision: 'Estandar',
+        color: 'Azul'
+    },
+
+    {
+        marca: 'Ferrari',
+        year: 2020,
+        minimo: 73000,
+        puertas: 2,
+        transmision: 'Automatico',
+        color: 'Negro'
+    },
+
+    {
+        marca: 'Lamborghini',
+        year: 2023,
+        minimo: 100000,
+        puertas: 2,
+        transmision: 'Automatico',
+        color: 'Amarillo'
+    },
+
+    {
+        marca: 'Mercedes Benz',
+        year: 2023,
+        minimo: 85000,
+        puertas: 2,
+        transmision: 'Automatico',
+        color: 'Blanco'
+    },
+
+    {
+        marca: 'Ford',
+        year: 2020,
+        minimo: 80000,
+        puertas: 4,
+        transmision: 'Estandar',
+        color: 'Amarillo'
+    },
+
+    {
+        marca: 'Dodge',
+        year: 2016,
+        minimo: 40000,
+        puertas: 4,
+        transmision: 'Estandar',
+        color: 'Azul'
+    },
+
+    {
+        marca: 'Volks Wagen',
+        year: 2017,
+        minimo: 60000,
+        puertas: 4,
+        transmision: 'Automatico',
+        color: 'Rojo'
+    },
+
+    {
+        marca: 'Kia',
+        year: 2019,
+        minimo: 61000,
+        puertas: 2,
+        transmision: 'Estandar',
+        color: 'Azul'
+    },
+
+    {
+        marca: 'Kia',
+        year: 2018,
+        minimo: 54000,
+        puertas: 4,
+        transmision: 'Estandar',
+        color: 'Naranja'
+    },
+
+    {
+        marca: 'Audi',
+        year: 2014,
+        minimo: 55000,
+        puertas: 2,
+        transmision: 'Estandar',
+        color: 'Negro'
+    },
+
+    {
+        marca: 'Ford',
+        year: 2021,
+        minimo: 73000,
+        puertas: 4,
+        transmision: 'Automatico',
+        color: 'Blanco'
+    },
+
+    {
+        marca: 'Ford',
+        year: 2017,
+        minimo: 45000,
+        puertas: 4,
+        transmision: 'Estandar',
+        color: 'Rojo'
+    },
+
+    {
+        marca: 'Ferrari',
+        year: 2024,
+        minimo: 100000,
+        puertas: 2,
+        transmision: 'Automatico',
+        color: 'Naranja'
+    },
+
+    {
+        marca: 'Nissan',
+        year: 2019,
+        minimo: 60000,
+        puertas: 4,
+        transmision: 'Estandar',
+        color: 'Azul'
+    },
+
+    {
+        marca: 'Dodge',
+        year: 2023,
+        minimo: 78000,
+        puertas: 4,
+        transmision: 'Automatico',
+        color: 'Amarillo'
+    },
+
+    {
+        marca: 'Nissan',
+        year: 2022,
+        minimo: 60000,
+        puertas: 2,
+        transmision: 'Estandar',
+        color: 'Azul'
+    },
+
+    {
+        marca: 'Volks Wagen',
+        year: 2022,
+        minimo: 75000,
+        puertas: 5,
+        transmision: 'Automatico',
+        color: 'Naranja'
+    },
+
+    {
+        marca: 'Lamborghini',
+        year: 2023,
+        minimo: 80000,
+        puertas: 2,
+        transmision: 'Automatico',
+        color: 'Blanco'
+    }
+]
